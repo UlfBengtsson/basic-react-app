0ac42e17912d6b0b6ea02305db3fea9299af1b1b
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-02-19 11:43:45 +0100

    state to prop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component, Fragment } from "react";
 import MyHeader from "./Components/MyHeader";
 import MyFooter from "./Components/MyFooter";
+import MyMainContent from "./Components/MyMainContent";
 
 class App extends Component {
+  state = {
+    codeLang: ["C#", "Java", "Basic", "C++", "Assembler"]
+  };
+
   render() {
     return (
       <Fragment>
         <MyHeader />
+
+        <MyMainContent textList={this.state.codeLang} />
+
         <MyFooter />
       </Fragment>
     );
