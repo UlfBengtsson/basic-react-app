89d0f93cd2d7c40bcec1c6612e9dc2dee80deb83
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-02-24 10:31:33 +0100

    added save to list

--- src/App.js
+++ src/App.js
@@ -5,8 +5,35 @@ import MyMainContent from "./Components/MyMainContent";
 
 class App extends Component {
   state = {
-    codeLang: ["C#", "Java", "Basic", "C++", "Assembler"]
+    codeLang: ["C#", "Java", "Basic", "C++", "Assembler"],
+    newLang: ""
   };
+
+  /*
+  handleNewLang = event => {
+    console.log(event.target.value);
+    const value = event.target.value;
+
+    this.setState({ newLang: value });
+
+  }
+  */
+  handleChange = event => {
+    const { name, value } = event.target
+  
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = () => {
+    
+    const newCodeLang = this.state.codeLang;
+
+    newCodeLang.push(this.state.newLang);
+
+    this.setState({codeLang: newCodeLang, newLang: ""});
+  }
 
   render() {
     return (
@@ -14,6 +41,11 @@ class App extends Component {
         <MyHeader />
 
         <MyMainContent textList={this.state.codeLang} />
+
+        <div>
+          <input name="newLang" type="text" value={this.state.newLang} onChange={this.handleChange} />
+          <button onClick={this.handleSubmit}>Save</button>
+        </div>
 
         <MyFooter />
       </Fragment>
